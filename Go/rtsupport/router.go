5521afe148d14b4e4c4199cdf062e14649ae111c
hugot305 <63977354+hugot305@users.noreply.github.com>
2020-04-20 09:23:23 -0400

    Add files via upload

---
+++ Go/rtsupport/router.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	r "github.com/dancannon/gorethink"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+// Handler ... 
+type Handler func(*Client, interface{})
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+//Router ...
+type Router struct {
+	rules   map[string]Handler
+	session *r.Session
+}
+
+//NewRouter ...
+func NewRouter(session *r.Session) *Router {
+	return &Router{
+		rules:   make(map[string]Handler),
+		session: session,
+	}
+}
+
+//Handle ...
+func (r *Router) Handle(msgName string, handler Handler) {
+	r.rules[msgName] = handler
+}
+
+//FindHandler ...
+func (r *Router) FindHandler(msgName string) (Handler, bool) {
+	handler, found := r.rules[msgName]
+	return handler, found
+}
+
+//ServeHTTP ...
+func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	client := NewClient(socket, e.FindHandler, e.session)
+	defer client.Close()
+	go client.Write()
+	client.Read()
+
+}
