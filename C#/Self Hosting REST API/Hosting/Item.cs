699c78f5b921b50cc84a8a734d2bb71d3018cc62
Hugo Trinidad <hugot305@gmail.com>
2021-06-10 09:54:05 -0400

    Adding C# projects

---
+++ C#/Self Hosting REST API/Hosting/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosting
+{
+     public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
